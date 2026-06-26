273b5723220ad15db6c7cbc68c9a77ebc2c4574e
alexgb23 <alexandergalvez880208@gmail.com>
2026-06-26 23:41:29 +0200

    quitar email

--- app/Http/Controllers/Api/ContactMessageController.php
+++ app/Http/Controllers/Api/ContactMessageController.php
@@ -3,11 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Mail\ContactMessageReceived;
 use App\Models\ContactMessage;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Mail;
 
 class ContactMessageController extends Controller
 {
@@ -28,26 +25,11 @@ class ContactMessageController extends Controller
             'status' => 'new',
         ]);
 
-        dispatch(function () use ($contactMessage) {
-            try {
-                Mail::to('alexandergalvez880208@gmail.com')
-                    ->send(new ContactMessageReceived($contactMessage));
-            } catch (\Throwable $e) {
-                Log::error('Mail failed after response', [
-                    'contact_message_id' => $contactMessage->id,
-                    'error' => $e->getMessage(),
-                    'class' => get_class($e),
-                ]);
-            }
-        })->afterResponse();
-
         return response()->json([
             'message' => 'Mensaje enviado correctamente',
             'data' => [
                 'id' => $contactMessage->id,
                 'status' => $contactMessage->status,
-                'mail_sent' => false,
-                'mail_queued_after_response' => true,
             ],
         ], 201);
     }
