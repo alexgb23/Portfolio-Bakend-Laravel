2343597368b0243328c6ba9fd6797238ab481f07
alexgb23 <alexandergalvez880208@gmail.com>
2026-06-30 13:29:01 +0200

    portfoliocontroller reestructurado para about

--- app/Http/Controllers/Api/PortfolioController.php
+++ app/Http/Controllers/Api/PortfolioController.php
@@ -3,23 +3,21 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\AboutResource;
 use App\Http\Resources\PortfolioHomeResource;
 use App\Models\ProfileHighlight;
-use App\Models\ProfileSetting;
 use App\Models\Skill;
 use App\Models\SocialLink;
 use App\Models\Project;
 use App\Models\Metric;
 use App\Models\Node;
 use App\Models\Server;
-
 use Illuminate\Http\Request;
 
 class PortfolioController extends Controller
 {
     public function getHomeData(Request $request): PortfolioHomeResource
     {
-
         $socialLinks = SocialLink::query()
             ->visible()
             ->ordered()
@@ -56,23 +54,21 @@ class PortfolioController extends Controller
         ]);
     }
 
-    public function getAboutData(Request $request): PortfolioHomeResource
+    public function getAboutData(Request $request): AboutResource
     {
-
-        $skills = Skill::query()
-            ->visible()
-            ->orderByDesc('is_featured')
-            ->ordered()
-            ->get();
+        // $skills = Skill::query()
+        //     ->visible()
+        //     ->orderByDesc('is_featured')
+        //     ->ordered()
+        //     ->get();
 
         $highlights = ProfileHighlight::query()
             ->visible()
             ->ordered()
             ->get();
 
-        return new PortfolioHomeResource([
-
-            'skills' => $skills,
+        return new AboutResource([
+            // 'skills' => $skills,
             'highlights' => $highlights,
         ]);
     }
