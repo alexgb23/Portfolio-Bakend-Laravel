73dc67c454a6ac5bcae819f70ca9cbb49438fa4d
alexgb23 <alexandergalvez880208@gmail.com>
2026-09-14 13:41:31 +0200

    metiendo imagen de firma en reenvio de mail

--- resources/views/emails/contact-confirmation.blade.php
+++ resources/views/emails/contact-confirmation.blade.php
@@ -1,24 +1,39 @@
 <x-mail::message>
-# ¡Hola, {{ $contactMessage->name }}!
+    # ¡Hola, {{ $contactMessage->name }}!
 
-Gracias por ponerte en contacto conmigo a través de mi portfolio web.
+    Gracias por ponerte en contacto conmigo a través de mi portfolio web.
 
-He recibido tu mensaje correctamente sobre el asunto: **"{{ $contactMessage->subject ?: 'Consulta Profesional' }}"**. Lo revisaré detenidamente y te responderé lo antes posible.
+    He recibido tu mensaje correctamente sobre el asunto: **"{{ $contactMessage->subject ?: 'Consulta Profesional' }}"**. Lo revisaré detenidamente y te responderé lo antes posible.
 
-Mientras tanto, te invito a seguir de cerca mi trabajo y conectar conmigo a través de mis redes oficiales:
+    Mientras tanto, te invito a seguir de cerca mi trabajo y conectar conmigo a través de mis redes oficiales:
 
-<x-mail::button :url="'https://www.linkedin.com/in/alexander-galvez-benavides-450917281/'" color="primary">
-Ver Perfil en LinkedIn
-</x-mail::button>
+    <x-mail::button :url="'https://www.linkedin.com/in/alexander-galvez-benavides-450917281/'" color="primary">
+        Ver Perfil en LinkedIn
+    </x-mail::button>
 
-<x-mail::button :url="'https://github.com/alexgb23'" color="success">
-Explorar Proyectos en GitHub
-</x-mail::button>
+    <x-mail::button :url="'https://github.com/alexgb23'" color="success">
+        Explorar Proyectos en GitHub
+    </x-mail::button>
 
-Un cordial saludo,
+    <x-mail::button :url="'https://alex.syskovex.com'" color="blue">
+        Ver Portfolio Profesional
+    </x-mail::button>
 
-**Alex | Syskovex**
-*Infrastructure & System Engineer*
-[alex.syskovex.com](https://alex.syskovex.com)
-*Barakaldo, País Vasco, España*
+    <x-mail::button :url="'https://syskovex.com'" color="gray">
+        Sitio Web Oficial
+    </x-mail::button>
+
+    Un cordial saludo,
+
+    **Alex | Syskovex**
+    *Infrastructure & System Engineer*
+
+    <div style="margin-top: 25px; margin-bottom: 10px; background-color: #030712; padding: 12px; width: fit-content; border-radius: 4px;">
+        <img src="https://syskovex.com/images/Tarjeta_FirmaDigital_Mail.png"
+            width="1138"
+            height="349"
+            style="display: block; width: 1138px; max-width: 100%; height: auto;"
+            alt="Alexander Galvez - Syskovex">
+    </div>
+
 </x-mail::message>
--- resources/views/emails/contact-message-received.blade.php
+++ resources/views/emails/contact-message-received.blade.php
@@ -1,17 +1,35 @@
 <x-mail::message>
-# Nuevo mensaje desde el portfolio
+    # Nuevo mensaje desde el portfolio
 
-**Nombre:** {{ $contactMessage->name }}
+    **Nombre:** {{ $contactMessage->name }}
 
-**Email:** {{ $contactMessage->email }}
+    **Email:** {{ $contactMessage->email }}
 
-**Asunto:** {{ $contactMessage->subject ?: 'Sin asunto' }}
+    **Asunto:** {{ $contactMessage->subject ?: 'Sin asunto' }}
 
-**Mensaje:**
+    **Mensaje:**
 
-{{ $contactMessage->message }}
+    {{ $contactMessage->message }}
 
-<x-mail::panel>
-Recibido: {{ $contactMessage->created_at?->format('d/m/Y H:i') }}
-</x-mail::panel>
+    <x-mail::panel>
+        Recibido: {{ $contactMessage->created_at?->format('d/m/Y H:i') }}
+    </x-mail::panel>
+
+    ---
+
+    **Enlaces del remitente:**
+
+    - LinkedIn: [Alexander Galvez](https://www.linkedin.com/in/alexander-galvez-benavides-450917281/)
+    - GitHub: [alexgb23](https://github.com/alexgb23)
+    - Portfolio: [alex.syskovex.com](https://alex.syskovex.com)
+    - Web: [syskovex.com](https://syskovex.com)
+
+    <div style="margin-top: 25px; margin-bottom: 10px; background-color: #030712; padding: 12px; width: fit-content; border-radius: 4px;">
+        <img src="https://syskovex.com/images/Tarjeta_FirmaDigital_Mail.png"
+            width="1138"
+            height="349"
+            style="display: block; width: 1138px; max-width: 100%; height: auto;"
+            alt="Alexander Galvez - Syskovex">
+    </div>
+
 </x-mail::message>
